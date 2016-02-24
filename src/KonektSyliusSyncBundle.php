940b7f44092eb2c8c5b5f8e59e0b59cda862d83a
Attila Fulop <fulopattila122@gmail.com>
2016-02-24 16:09:05 +0200

    Added bundle boot class

---
+++ src/KonektSyliusSyncBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Contains class KonektSyliusSyncBundle
+ *
+ * @copyright   Copyright (c) 2016 Storm Storez Srl
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2016-02-24
+ * @version     2016-02-24
+ */
+
+namespace Konekt\SyliusSyncBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KonektSyliusSyncBundle extends Bundle
+{
+
+}
