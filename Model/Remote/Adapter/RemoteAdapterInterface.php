0b443da9a7f3a88cc8edb4957780477b67a60c70
Sandor Teglas <sandor@artkonekt.com>
2016-03-02 13:57:04 +0200

    Extended the RemoteAdapterInterface with the fetchStocks() method.

--- Model/Remote/Adapter/RemoteAdapterInterface.php
+++ Model/Remote/Adapter/RemoteAdapterInterface.php
@@ -3,9 +3,10 @@
  * RemoteAdapterInterface.php
  *
  * @author      Attila Fulop
+ * @author      Sandor Teglas
  * @copyright   Copyright (c) 2016 Storm Storez Srl-d
  * @license     Proprietary
- * @version     2016-02-26
+ * @version     2016-03-02
  * @since       2016-02-25
  */
 
@@ -13,13 +14,17 @@
 namespace Konekt\SyliusSyncBundle\Model\Remote\Adapter;
 
 
+use Konekt\SyliusSyncBundle\Model\Remote\Product\RemoteProductInterface;
+use Konekt\SyliusSyncBundle\Model\Remote\Stock\RemoteStockInterface;
+use Konekt\SyliusSyncBundle\Model\Remote\Taxonomy\RemoteTaxonomyInterface;
+
 interface RemoteAdapterInterface
 {
 
     /**
      * Returns a list of products to be imported
      *
-     * @return Konekt\SyliusSyncBundle\Model\Remote\Product\RemoteProductInterface[]
+     * @return RemoteProductInterface[]
      */
     public function fetchProducts();
 
@@ -27,17 +32,23 @@ interface RemoteAdapterInterface
     /**
      * Returns a list of taxonomies to be imported
      *
-     * @return Konekt\SyliusSyncBundle\Model\Remote\Taxonomy\RemoteTaxonomyInterfaceInterface[]
+     * @return RemoteTaxonomyInterface[]
      */
     public function fetchTaxonomies();
 
+    /**
+     * Returns a list of Stocks to be imported
+     *
+     * @return RemoteStockInterface[]
+     */
+    public function fetchStocks();
 
     /**
      * Returns a certain taxonomy to be imported
      *
      * @param   string  $id     The id/code of the taxonomy
      *
-     * @return Konekt\SyliusSyncBundle\Model\Remote\Taxonomy\RemoteTaxonomyInterfaceInterface
+     * @return  RemoteTaxonomyInterface
      */
     public function fetchTaxonomy($id);
 
